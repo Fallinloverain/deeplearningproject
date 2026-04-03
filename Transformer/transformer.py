22784c17b8913bb89f5f7b444d7ac7e8d65eab36
Fallinloverain <allenloverain@gmail.com>
2026-04-04 00:13:21 +1300

    code1

--- Transformer/transformer.py
+++ Transformer/transformer.py
@@ -2,3 +2,47 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import math
+
+#batch, time, demension
+X = torch.randn(128, 64, 512) 
+print("Input size:", X.shape)
+
+#初始化参数
+d_model = 512
+n_head = 8
+
+class multi_head_attention(nn.Module):
+    def __init__(self, d_model, n_head):
+        super(multi_head_attention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_combine = nn.Linear(d_model, d_model)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, v, mask):
+        batch, time, dimension = q.shape
+        q = self.w_q(q)
+        k = self.w_k(k)
+        v = self.w_v(v)
+
+        n_d = self.d_model//self.n_head
+
+        q = q.view(batch, time, self.n_head, n_d).permute(0,2,1,3)
+        k = k.view(batch, time, self.n_head, n_d).permute(0,2,1,3)
+        v = v.view(batch, time, self.n_head, n_d).permute(0,2,1,3)
+
+        score = q @ k.transpose(2,3)/ math.sqrt(n_d)
+        if mask is not None:
+            mask = torch.tril(torch.ones(time, time, dtype=bool))
+            score = score.masked_fill(mask == 0, float("-inf"))
+        
+        score = self.softmax(score) @ v
+
+        score = score.permute(0,2,1,3).contiguous().view(batch, time, dimension)
+
+        output = self.w_combine(score)
+        return output
