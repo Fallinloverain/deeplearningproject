58ae76330a0277d78fb62871db26c1ff973c3cd2
Fallinloverain <allenloverain@gmail.com>
2026-04-14 23:58:16 +1200

    update 14/04/2026

--- Transformer/Embedding.py
+++ Transformer/Embedding.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 
 
-#做的是将词id转化为向量(vocab_size为词汇表大小，d_model为词向量长度)
+#做的是将词id转化为向量(vocab_size为词汇表大小，d_model为Token词向量长度)
 class TokenEmbedding(nn.Embedding):
     def __init__(self, vocab_size, d_model):
         super(TokenEmbedding, self).__init__(
@@ -32,3 +32,52 @@ class PositionalEmbedding(nn.Module):
     def forward(self, x):
         seq_len = x.shape[1]
         return self.encoding[:seq_len, :]
+    
+
+class TransformerEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model, Maxlen, dropout, device):
+        super(TransformerEmbedding, self).__init__()
+        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        self.pos_emb = PositionalEmbedding(d_model, Maxlen, device)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.tok_emb(x) + self.pos_emb(x)
+        x = self.dropout(x)
+        return x
+
+
+
+
+
+
+#d_model为Token词向量长度，Transformer 把“每个 token”当作一个独立样本，layernorm的作用是对每个样本进行归一化，保证每个样本的均值为0，方差为1，增强模型的稳定性和收敛速度
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-10):
+        super(LayerNorm,self).__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+    
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, unbiased=False, keepdim=True)
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = self.gamma * out + self.beta
+        return out
+
+#前馈网络，输入输出维度都是d_model，hidden为前馈网络的隐藏层维度, 通常 4×d_model，dropout为dropout率
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, hidden, dropout=0.1):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, hidden)
+        self.fc2 = nn.Linear(hidden, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
